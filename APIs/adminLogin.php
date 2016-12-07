f7f127f5611c389452206f052aaffbbac106e746
mikecit22 <mikecit22@gmail.com>
2016-12-06 22:09:43 -0500

    updated sql

--- APIs/adminLogin.php
+++ APIs/adminLogin.php
@@ -5,8 +5,8 @@
 	$password = $_POST['password'];
 
 	$sql = "SELECT *
-			FROM reader 
-			WHERE  id = $id and readertype = 'admin' and password = $password";
+			FROM readers 
+			WHERE  readerid = $id and readertype = 'admin' and password = $password";
 
 	$results = mysqli_fetch_assoc($conn->query($sql));
 	print json_encode($results);
