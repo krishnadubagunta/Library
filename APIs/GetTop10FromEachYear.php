5df42de1f0008e9937978ab0eb16a7ae73b1e21d
mikecit22 <mikecit22@gmail.com>
2016-12-10 18:06:21 -0500

    asf

--- APIs/GetTop10FromEachYear.php
+++ APIs/GetTop10FromEachYear.php
@@ -7,7 +7,7 @@
 	$sql = "SELECT count(borrows.bornumber) as borrowCount, docuemnt.title, docuemnt.docid, YEAR(borrows.bdtime) as year
 			FROM docuemnt, borrows
 			WHERE borrows.docid = docuemnt.docid
-			GROUP BY year
+			GROUP BY year, docuemnt.docid
 			ORDER BY year DESC";
 
 	$jsonHack = false;
