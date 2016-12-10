2c0bf4087859b3010194909b5b14b05ed2c31be8
mikecit22 <mikecit22@gmail.com>
2016-12-10 15:51:35 -0500

    upade

--- APIs/Return.php
+++ APIs/Return.php
@@ -3,7 +3,7 @@
 
 	$bornumber = $_POST['bornumber'];
 
-	$sql = "UPDATE `borrows` SET `rdtime` = CURRENT_TIME() WHERE `borrows`.`bornumber` = $bornumber;";
+	$sql = "UPDATE `borrows` SET `rdtime` = '".date('Y-m-d')."' WHERE `borrows`.`bornumber` = $bornumber;";
 
 	$query = mysqli_query($conn, $sql);
 	print_r($sql); 
